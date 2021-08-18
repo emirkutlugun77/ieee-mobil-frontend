46c30bd83711abeedcdd2cc94ef4f84b8dd529d3
emir <emirkutlugun01@gmail.com>
2021-08-18 18:53:16 +0300

    açıklama2

--- lib/Functions/blog.dart
+++ lib/Functions/blog.dart
@@ -7,16 +7,21 @@ import 'package:my_app/UI/models/blogposts.dart';
 const baseUri = 'https://ancient-falls-28306.herokuapp.com/';
 
 Future getMost5(List<BlogPost> blogPosts, String token, int page) async {
+// api dan cevap alma
   var response = await http.get(
       Uri.parse(baseUri + 'v1/blog-post?size=6&page=$page'),
       headers: {HttpHeaders.authorizationHeader: 'Bearer $token'});
-
+// api dan cevap alma burdaki header kısmı her apida farklı olabilir
   if (response.statusCode == 200) {
+    // yaptığım api request başarılı olduysa statusCode 200 olur
     var decodedData = jsonDecode(response.body);
-
+    //üstteki satırda aldığım dönütü JSON formatına çeviriyorum
     decodedData['blogPosts'].forEach((e) async {
       blogPosts.add(BlogPost.fromJson(e));
     });
+    //json formatına bağlı olarak tek tek her data parçasını client side kısmında yazdığım modeller yardımıyla çevirip kendime alıyorum
+    //örnek modeller için models klasörüen bak
+    //burdaki kullanım pek doğru değil sen istersen return blogPosts da yazabilirsin
   } else {
     return 'error';
   }
